9452bebc27b3d259b9a93fc3117be979d950dccd
Alonso <cardenasjairods@gmail.com>
2022-10-08 16:17:22 -0500

    uploading data json, final one is its.json

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const { getListings } = require("./functions/getListings");
+const { getListings, getListingsFromOpenSea, getIt, repair, repairUnder } = require("./functions/getListings");
 
 (async ()=>{
-    await getListings();
+    //await getListings();
+    //await getListingsFromOpenSea();
+    const its = await getIt();
+    //console.log(its);
+    //await repairUnder();
 })();
