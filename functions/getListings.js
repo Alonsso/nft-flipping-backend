d77b05987cdd11d53688bc19e86baa7bb7d871d6
Alonso <cardenasjairods@gmail.com>
2022-10-07 19:30:43 -0500

    load

---
+++ functions/getListings.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+const getListings = async () => {
+    let cnt = 0;
+    let response = null;
+    let url = 'https://services.itrmachines.com/test-opensea/service/getData?collection=0x913ae503153d9a335398d0785ba60a2d63ddb4e2?token_id=12'
+    do {
+        try {
+            
+        } catch (error) {
+            
+        }
+    } while (cnt );
+}
+
+module.exports = { getListings };
